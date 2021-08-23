f43322bf0d833fa9941d9b5292ffa0d25f1660e6
Anisur Rahman <anisurlist3@gmail.com>
2021-08-23 08:45:14 +0600

    vue3 with vite, tailwind@2.2.7 install

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" class="bg-red-500 rounded text-white focus:ring focus:ring-red-300 px-3 py-1" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
+  <p>
+    Edit
+    <code class="font-sans bg-gray-200">components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
